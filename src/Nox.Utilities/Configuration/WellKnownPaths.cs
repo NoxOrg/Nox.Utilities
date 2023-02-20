fa023c9344ca2536d38b252d37a829e325ec0349
Andre Sharpe <andre.sharpe@gmail.com>
2023-02-20 19:40:10 +0100

    Moved json token+secrets to store

--- src/Nox.Utilities/Configuration/WellKnownPaths.cs
+++ src/Nox.Utilities/Configuration/WellKnownPaths.cs
@@ -8,9 +8,10 @@ namespace Nox.Utilities.Configuration;
 
 public static class WellKnownPaths
 {
-    public static readonly string CliCachePath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "nox");
-    public static readonly string CachePath = Utilities.Configuration.WellKnownPaths.CliCachePath;
+    private static readonly string _userApps = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
+    public static readonly string CachePath = Path.Combine(_userApps, "nox");
     public static readonly string CacheFile = Path.Combine(CachePath, "NoxCliCache.json");
-    public static readonly string CacheTokenFile = Path.Combine(CachePath, "NoxCliCache-token.json");
     public static readonly string WorkflowsCachePath = Path.Combine(CachePath, "workflows");
+    public static readonly string SecretsCachePath = Path.Combine(CachePath, "store");
+    public static readonly string CacheTokenFile = Path.Combine(SecretsCachePath, "NoxCliCache-token.json");
 }
