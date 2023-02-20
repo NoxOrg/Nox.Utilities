f78a92acd2f0b6a371f0c9080bd1169824f6a474
Andre Sharpe <andre.sharpe@gmail.com>
2023-02-20 17:56:41 +0100

    Added welknown paths for caching

---
+++ src/Nox.Utilities/Configuration/WellKnownPaths.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nox.Utilities.Configuration;
+
+public static class WellKnownPaths
+{
+    public static readonly string CliCachePath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "nox");
+    public static readonly string CachePath = Utilities.Configuration.WellKnownPaths.CliCachePath;
+    public static readonly string CacheFile = Path.Combine(CachePath, "NoxCliCache.json");
+    public static readonly string CacheTokenFile = Path.Combine(CachePath, "NoxCliCache-token.json");
+    public static readonly string WorkflowsCachePath = Path.Combine(CachePath, "workflows");
+}
